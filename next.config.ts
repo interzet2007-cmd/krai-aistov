9e61c427bd860a9db213feb45a17f2a0d3727def
interzet2007-cmd <interzet2007@gmail.com>
2026-07-16 14:06:07 +0300

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/krai-aistov",
+  assetPrefix: "/krai-aistov",
+  trailingSlash: true,
+  images: { unoptimized: true },
+};
+
+export default nextConfig;
+
